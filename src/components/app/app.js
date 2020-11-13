e0d69bc9e3199f755ef64946e9fae905f8cad23a
Doomer77 <newzibkoff1984@gmail.com>
2020-11-13 23:06:26 +0300

    add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,9 @@ export default class App extends Component {
       this.createTodoItem('Drink Coffee'),
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch')
-    ]
+    ],
+    term:'',
+    filter: 'active'
   };
 
   deletedItem = (id) => {
@@ -84,10 +86,27 @@ export default class App extends Component {
     });
   };
 
+  onSearchChange = (term) => {
+    this.setState({
+      term
+    });
+  };
+
+  search (items, term) {
+     if (term === 0) {
+      return items;
+     }
+    return items.filter((item) => {
+      return item.label.toLowerCase()
+      .indexOf(term.toLowerCase()) > -1; 
+    });
+  };
+
   render () {
 
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
 
+    const visibleItems = this.search(todoData, term);
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
 
@@ -95,14 +114,16 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onSearchChange={ this.onSearchChange }/>
           <ItemStatusFilter />
         </div>
   
-        <TodoList todos={todoData}
-        onDeleted={ this.deletedItem } 
-        onToggleImportant={this.onToggleImportant}
-        onToggleDone={this.onToggleDone}/>
+        <TodoList 
+          todos={visibleItems}
+          onDeleted={ this.deletedItem } 
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone}
+        />
 
         <ItemAddForm onItemAdded={this.addItem}/>
       </div>
